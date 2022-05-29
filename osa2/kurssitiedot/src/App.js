a167289fede6cb0e742d57858e377c26744549fc
pekka <pekka.paldanius@kotiposti.net>
2022-05-29 12:52:47 +0300

    Otsikko lisätty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -4,6 +4,7 @@ import Course from './components/Course'
 const App = ({ courses }) => {
     return (
       <div>
+        <h1>Web development curriculum</h1>
           {courses.map(course =><Course key={course.id} course={course}/>)}  
       </div>
     )
