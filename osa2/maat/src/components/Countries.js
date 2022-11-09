d995faa1cef48371858ee21f06a8794f9d6dcf40
pekka <pekka@P55MGD45.35>
2022-11-09 18:00:39 +0200

    tehtävä 2.14 tehty

--- osa2/maat/src/components/Countries.js
+++ osa2/maat/src/components/Countries.js
@@ -1,9 +1,9 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Country from './Country'
 
-
 const Countries = (props) => {
-    const [countryToBeShown, setCountryToBeShown] = useState('')
+    const [countryToBeShown, setCountryToBeShown] = useState('');
+    const [weather, setWeather] = useState([]);
 
     const getInfo = (event) => {
         let name = "";
@@ -14,6 +14,8 @@ const Countries = (props) => {
     }
 
     let countriesToShow;
+    let latitude;
+    let longitude;
 
     if (countryToBeShown) {
         countriesToShow = props.countries.filter(country => country.name.official === countryToBeShown);
@@ -21,9 +23,28 @@ const Countries = (props) => {
         countriesToShow = props.countries.filter(country => country.name.official.includes(props.filterCountries));
     }
 
+    if (countriesToShow.length > 0) {
+        latitude = countriesToShow[0].capitalInfo.latlng[0];
+        longitude = countriesToShow[0].capitalInfo.latlng[1];
+    }
+
+    useEffect(() => {
+        if (latitude && longitude) {
+                fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&hourly=temperature_2m,windspeed_10m")
+                .then((response) => response.json())
+                .then((data) => {
+                    let result = []
+                    result.push(data.hourly.temperature_2m[0]);
+                    result.push(data.hourly.windspeed_10m[0]);
+                    setWeather(result);
+                })
+            } else {
+                    setWeather([]);
+            }
+        }, [latitude, longitude]);
+
 
     if (countriesToShow.length === 1) {
-        console.log("countriesToShow[0].flags.pgn: ", countriesToShow[0].flags.png)
         return (
             <div>
             <h2>{countriesToShow[0].name.official}</h2>
@@ -32,8 +53,11 @@ const Countries = (props) => {
             <h4>languages:</h4>
             {Object.values(countriesToShow[0].languages).map(language => <li key={language}>{language}</li>)}
             <img src={countriesToShow[0].flags.png} alt="flag"/>
+            <h2>Weather in {countriesToShow[0].capital[0]}</h2>
+            temperature {weather[0]} Celsius
+           <br/>
+            wind {weather[1]} m/s
             </div>
-
         )
     } else if (countriesToShow.length < 10) {
     return (
